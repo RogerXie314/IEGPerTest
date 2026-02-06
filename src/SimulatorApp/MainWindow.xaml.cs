03d93844bc38acce527bd07e0b44ca8c412d69c1
IEGDev <dev@local>
2026-02-06 18:24:08 +0800

    ui: improve layout, grouping and alignment in MainWindow

--- src/SimulatorApp/MainWindow.xaml.cs
+++ src/SimulatorApp/MainWindow.xaml.cs
@@ -1,35 +1,13 @@
-using System;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using SimulatorLib.Network;
-using SimulatorLib.Workers;
 
 namespace SimulatorApp
 {
     public partial class MainWindow : Window
     {
-        private CancellationTokenSource? _hbCts;
-
         public MainWindow()
         {
-            using System.Windows;
-            using SimulatorApp.ViewModels;
-
-            namespace SimulatorApp
-            {
-                public partial class MainWindow : Window
-                {
-                    public MainWindow()
-                    {
-                        InitializeComponent();
-                        DataContext = new MainViewModel();
-                    }
-                }
-            }
-            }
-            catch { return false; }
+            InitializeComponent();
+            DataContext = new ViewModels.MainViewModel();
         }
     }
 }
